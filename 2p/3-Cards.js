90080a3b1bfb0bc659c629764fc50bbf8c4b5ead
Ernesto2899 <50375286+Ernesto2899@users.noreply.github.com>
2019-10-18 18:27:54 -0500

    Update 3-Cards.js

--- 2p/3-Cards.js
+++ 2p/3-Cards.js
@@ -1,109 +1,106 @@
-class Cards {
-    printC(num){
-        let MCard = []
+class Card {
+    printCard(num){
+        let myCard = []
 
         if (num <= 13) {
-            MCard[0] = 0
-       
+            myCard[0] = 0
         } else if (num <= 26) {
-            MCard[0] = 1
-       
+            myCard[0] = 1
         } else if (num <= 39) {
-            MCard[0] = 2
-       
+            myCard[0] = 2
         } else {
-            MCard[0] = 3
+            myCard[0] = 3
         }
 
         switch (num % 13) {
-           
-                 case 0:
-                MCard[1] = 13
+            case 0:
+                myCard[1] = 13
                 break
-            
-                case 1:
-                MCard[1] = 1
+            case 1:
+                myCard[1] = 1
                 break
-           
-                case 2:
-                MCard[1] = 2
+            case 2:
+                myCard[1] = 2
                 break
-            
-                case 3:
-                MCard[1] = 3
+            case 3:
+                myCard[1] = 3
                 break
-           
-                case 4:
-                MCard[1] = 4
+            case 4:
+                myCard[1] = 4
                 break
-            
-                case 5:
-                MCard[1] = 5
+            case 5:
+                myCard[1] = 5
                 break
-           
-                case 6:
-                MCard[1] = 6
+            case 6:
+                myCard[1] = 6
                 break
-           
-                case 7:
-                MCard[1] = 7
+            case 7:
+                myCard[1] = 7
                 break
-            
-                case 8:
-                MCard[1] = 8
+            case 8:
+                myCard[1] = 8
                 break
-           
-                case 9:
-                MCard[1] = 9
+            case 9:
+                myCard[1] = 9
                 break
-            
-                case 10:
-                MCard[1] = 10
+            case 10:
+                myCard[1] = 10
                 break
-            
-                case 11:
-                MCard[1] = 11
+            case 11:
+                myCard[1] = 11
                 break
-           
-                case 12:
-                MCard[1] = 12
-
+            case 12:
+                myCard[1] = 12
             break
         }
 
-        return MCard
+        return myCard
 
     }
 
-    PrintNum(x, z){
-        
-        let MCard = 0
-       
-        if (x == 0) {
-           
-            MCard = z
-       
-        } else if (x == 1){
-            
-            MCard = z + 13
-        
-        } else if (x == 2){
-           
-            MCard = z + 26
-       
-        } else if (x == 3){
-           
-            MCard = z + 39
-        
-        } else MCard = "-Error-"
+    printNumber(k, n){
+        let myCard = 0
+        if (k == 0) {
+            myCard = n
+        } else if (k == 1){
+            myCard = n + 13
+        } else if (k == 2){
+            myCard = n + 26
+        } else if (k == 3){
+            myCard = n + 39
+        } else myCard = "-Error-"
 
-        return MCard
+        return myCard
     }
-}  
+
+    onePair(){
+
+        function factorial(n) {
+            return n <= 1 ? 1 : n * factorial(n - 1);
+        }
+               
+        let primer= (factorial(13) / (factorial(13-1)*(factorial(1))));
+      
+        let seg= (factorial(4) / (factorial(4-2)*(factorial(2))))
+   
+        let tercer= (factorial(12) / (factorial(12-3)*(factorial(3))))
+     
+        let cuartoFir=(factorial(4) / (factorial(4-1)*(factorial(1))))
+        let cuatro= Math.pow(cuartoFir,3);
+
+        let num_posibles=(factorial(52) / (factorial(52-5)*(factorial(5))))
+        let operacion= primer*seg*tercer*cuatro;
+        let prob=operacion/num_posibles;
+       
+
+        return ((prob.toFixed(4))*100)+"%";
+    }
+}
+
 
 ( function useCard() {
-    c1 = new Cards
-    
-    console.log(c1.printC(21))
-    console.log(c1.PrintNum(3,6))
+    c1 = new Card
+    console.log(c1.printCard(29))
+    console.log(c1.printNumber(2,3))
+    console.log(c1.onePair())
 })()
