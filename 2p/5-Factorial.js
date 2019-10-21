d6f49db6b3e01f7c5a864c96b1e9f9fe8f5a93bd
Ernesto2899 <50375286+Ernesto2899@users.noreply.github.com>
2019-10-21 01:14:56 -0500

    Update 5-Factorial.js

--- 2p/5-Factorial.js
+++ 2p/5-Factorial.js
@@ -1,44 +1,65 @@
-class Factorial{
-    constructor(){
-       
-        this.cost = null
-        this.succession = []
-        this.tabla = []
+class Factorial {
+    constructor(n) {
+        this.n = n;
     }
+    Value(n= this.n ) {
 
-    Value(x){
-    
-        this.x = x
-    
-        this.succession[this.cost] = x
-    
-        this.cost +=1
-    
-        this.tabla[this.cost]= this.cost
-   
-        return x<2 ? 1 : this.x*this.Value(X-1);
+        function value(n) {
+
+            if (n <= 1) {
+
+               return 1;
+
+            } else {
+
+                return n * value(n - 1);
+            }
+        }
+        return value(n);
     }
+    Cost( n = this.n ) {
 
-    IntCost(){
-        return this.cost;
-   
+        let cont = 0;
+
+        function value(n) {
+
+            if (n <= 1) {
+
+                cont++;
+            } else {
+
+                cont++;
+
+                return n * value(n - 1);
+            }
+        }
+        value(n);
+
+        return cont;
     }
+    Sucesion() {
 
-    VectorSuc(){
-        return this.succession;
-    
+        let sucesion = [];
+
+        for (let i = 1; i <= this.n; i++) {
+
+            sucesion.push(this.Value(i));
+        }
+        return sucesion;
     }
+    Table() {
 
-    VectorTable(){
-        return this.table;
-   
+        console.log("Number-Cost-Value ");
+
+        for (let i = 1; i <= this.n; i++) {
+
+            console.log("X(" + i + ") - " + this.Cost(i) + " - " + this.Value(i));
+        }
     }
 }
+let Fact = new Factorial(7);
 
-let A = new Factorial
-
-
-A.Value(8)
-A.IntCost();
-A.VectorSuc();
-A.VectorTable();
+console.log(Fact.Value());
+console.log(Fact.Sucesion());
+console.log(Fact.Cost());
+console.log(Fact.Table());
